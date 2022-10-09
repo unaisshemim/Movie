58418a45cd853e9a0f4f72ef38acbca97d93141b
unaisshemim <unaisshemim@gmail.com>
2022-10-09 08:31:42 +0530

    search optimized

--- src/pages/product/Products.jsx
+++ src/pages/product/Products.jsx
@@ -1,52 +1,42 @@
+import React, { useEffect, useState } from "react";
 
-import React, { useEffect } from 'react'
+import { useDispatch, useSelector } from "react-redux";
+import Navbar from "../../components/navbar/Navbar";
+import MediaCard from "../../components/productsList/ProductList";
+import { getProducts } from "../../redux/getApi";
+import "./products.scss";
+function Products() {
+  const dispatch = useDispatch();
 
-import { useDispatch, useSelector } from 'react-redux'
-import Navbar from '../../components/navbar/Navbar'
-import MediaCard from '../../components/productsList/ProductList'
-import { getProducts } from '../../redux/getApi'
-import './products.scss'
-function Products( ) {
+  const movies = useSelector((state) => state.product.currentProduct);
+  const search = useSelector((state) => state.product.searchProduct);
 
-const dispatch=useDispatch()
+  const [fetch, setFetch] = useState(false);
+  useEffect(()=>{
+    if(search===""){
+      setFetch(false)
+    }
+  },[search])
 
-const movies=useSelector(state=>state.product.currentProduct)
-const search=useSelector(state=>state.product.searchProduct)
+  const filteredProduct = movies.filter((value) => {
+    return value.title.toLowerCase().includes(search.toLowerCase());
+  });
 
-
-const filteredProduct = movies.filter(
-  value => {
-    return (
-      value
-      .title
-      .toLowerCase()
-      .includes(search.toLowerCase()) 
-    );
-  }
-);
-
-useEffect(()=>{
-getProducts(dispatch)
-  return ()=>{
-
-getProducts(dispatch)
-  }
-},[dispatch])
-
-
-
-
+  useEffect(() => {
+    getProducts(dispatch);
+    return () => {
+      getProducts(dispatch);
+    };
+  }, [dispatch]);
 
   return (
-    <div className='products'>   
-    <Navbar/>  
-        {filteredProduct?.map((value,key)=>{
-          return <MediaCard key={key} value={value}/>
-        })
-      }
-     
+    <div className="products">
+      <Navbar />
+      {fetch?movies:filteredProduct?.map((value, key) => {
+        return <MediaCard key={key} value={value} />;
+      })}
     </div>
-  )
+  );
 }
 
-export default Products
+export default Products;
